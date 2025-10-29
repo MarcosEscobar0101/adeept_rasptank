a4b818b7145688bfa8902305d3bdba651251255c
escobar <itatm@rapixchange.com>
2025-10-29 15:28:25 -0600

    modificacion ultrasony

--- server/ultra.py
+++ server/ultra.py
@@ -1,61 +1,63 @@
 #!/usr/bin/python3
-# File name   : Ultrasonic.py
-# Description : Detection distance and tracking with ultrasonic
-# Website     : www.gewbot.com
-# Author      : William
-# Date        : 2019/02/23
+# Ultrasonic (BCM23=TRIG, BCM24=ECHO) con timeouts y media robusta
+
 import RPi.GPIO as GPIO
 import time
+from statistics import median
 
-Tr = 11
-Ec = 8
+# Pines (BCM)
+Tr = 23  # TRIG  (físico 16)
+Ec = 24  # ECHO  (físico 18)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(Tr, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Ec, GPIO.IN)
 
-def checkdist():       #Reading distance
-    for i in range(5):  # Remove invalid test results.
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
-        GPIO.setup(Ec, GPIO.IN)
-        GPIO.output(Tr, GPIO.LOW)
-        time.sleep(0.000002)
-        GPIO.output(Tr, GPIO.HIGH)
-        time.sleep(0.000015)
-        GPIO.output(Tr, GPIO.LOW)
-        while not GPIO.input(Ec):
-            pass
-        t1 = time.time()
-        while GPIO.input(Ec):
-            pass
-        t2 = time.time()
-        dist = (t2-t1)*340/2
-        if dist > 9 and i < 4:  # 5 consecutive times are invalid data, return the last test data
-            continue
-        else:
-            return (t2-t1)*340/2
+def pulse_distance(timeout_s=0.03):
+    """Mide una vez con timeouts; devuelve distancia en metros o None"""
+    # pulso ~10-15us
+    GPIO.output(Tr, GPIO.LOW)
+    time.sleep(0.000002)
+    GPIO.output(Tr, GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(Tr, GPIO.LOW)
 
-# def checkdist():       #Reading distance
-#     GPIO.setmode(GPIO.BCM)
-#     GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
-#     GPIO.setup(Ec, GPIO.IN)
-#     GPIO.output(Tr, GPIO.HIGH)
-#     time.sleep(0.000015)
-#     GPIO.output(Tr, GPIO.LOW)
-#     while not GPIO.input(Ec):
-#         pass
-#     t1 = time.time()
-#     while GPIO.input(Ec):
-#         pass
-#     t2 = time.time()
-#     return round((t2-t1)*340/2,2)
-#     #return (t2-t1)*340/2
+    t0 = time.time()
+    # esperar flanco de subida en ECHO
+    while not GPIO.input(Ec):
+        if time.time() - t0 > timeout_s:
+            return None
+    t1 = time.time()
+    # esperar flanco de bajada en ECHO
+    while GPIO.input(Ec):
+        if time.time() - t1 > timeout_s:
+            return None
+    t2 = time.time()
+
+    # velocidad del sonido ~340 m/s
+    return (t2 - t1) * 340.0 / 2.0
+
+def checkdist(samples=5):
+    """Devuelve la mediana en metros de varias mediciones válidas"""
+    vals = []
+    for _ in range(samples):
+        d = pulse_distance()
+        if d is not None:
+            vals.append(d)
+        time.sleep(0.01)
+    if not vals:
+        return None
+    return median(vals)
 
 if __name__ == '__main__':
-    while True:
-        distance = checkdist()*100
-        print("%.2f cm" %distance)
-        time.sleep(1)
+    try:
+        while True:
+            d = checkdist()
+            if d is None:
+                print("Sin lectura")
+            else:
+                print(f"{d*100:.2f} cm")
+            time.sleep(0.2)
+    finally:
+        GPIO.cleanup((Tr, Ec))
